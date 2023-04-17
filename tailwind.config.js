8e3a4f4b8157e3de1dff1ce01e8d5d1189dbc6d5
Claudia Lisboa <ccalumby@gmail.com>
2023-04-17 01:03:32 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
